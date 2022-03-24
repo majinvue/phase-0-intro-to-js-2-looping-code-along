78e924ef995e8c70abd437383783ec57742040fd
lloydvue <lloydvue94@gmail.com>
2022-03-23 19:58:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const myName = []
+    for(let i = 0; i < names.length; i++){
+        myName.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return myName
+}
+
+function countDown(i){
+    while(i >= 0){
+        console.log(i--)
+    }
+}
